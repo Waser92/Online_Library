2546412f9b0093e253f2e3c97974f0f7978271c3
Thomas.pl <thomas.planche92@gmail.com>
2024-03-12 09:50:21 +0100

    Update

--- Branch_page/Branch.js
+++ Branch_page/Branch.js
@@ -1,4 +1,11 @@
+var Data_Json = 'C/Data/Main_data.json';
 
-document.getElementById("Description").innerText ="Description des misérables";
+// Maintenant, vous pouvez accéder à vos données JSON à partir de la variable donneesJson
+var NomOeuvre = Data_Json.Les_Misérables.nom_oeuvre;
+var Description = Data_Json.Les_Misérables.Description;
+var Url = Data_Json.Les_Misérables.URL;
 
-document.getElementById("Description").innerHTML ="Description des misérables";
+// Vous pouvez ensuite utiliser ces variables comme bon vous semble
+console.log("Nom de l'oeuvre:", nomOeuvre);
+console.log('Description:', description);
+console.log('URL:', url);
