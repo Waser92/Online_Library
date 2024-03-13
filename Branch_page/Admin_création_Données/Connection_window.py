f7e40dd8cca7b14fc10d97daf460bb9851bc65aa
Thomas.pl <thomas.planche92@gmail.com>
2024-03-13 11:11:15 +0100

    Update Gestion Données

--- Branch_page/Admin_création_Données/Connection_window.py
+++ Branch_page/Admin_création_Données/Connection_window.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 
 # Importation du module Gestion_data_user depuis le package
-from Gestion_data_user import GestionUserData
+from Gestion_data_user import GestionData
 
 class MyWindow_connection:
 
@@ -16,7 +16,7 @@ class MyWindow_connection:
         self.mot_de_passe_var = StringVar()
 
         # Nouvelle instance de GestionUserData
-        self.user_data_manager = GestionUserData()
+        self.user_data_manager = GestionData()
 
         self.frame_top = Frame(self.window, bg='#030720')
         self.frame_center = Frame(self.window, bg='#030720')
@@ -35,12 +35,12 @@ class MyWindow_connection:
         self.create_buttons()
 
     def create_title(self):
-        label_title = Label(self.frame_top, text="\n Création d'une nouvelle page\n ", font=("Courier", 40), bg='#030720',
+        label_title = Label(self.frame_top, text="\n Création d'une nouvelle page\n ", font=("Courier", 35), bg='#030720',
                             fg='white')
         label_title.pack()
 
     def create_subtitle(self):
-        label_subtitle = Label(self.frame_top, text="Entrez les données qui seront affichées", font=("Courier", 25), bg='#030720',
+        label_subtitle = Label(self.frame_top, text="Entrez les données \n qui seront affichées", font=("Courier", 25), bg='#030720',
                                fg='white')
         label_subtitle.pack()
 
@@ -62,7 +62,7 @@ class MyWindow_connection:
         style = ttk.Style()
         style.configure("TButton", padding=(20, 10))
 
-        bouton_valider = ttk.Button(self.frame_center, text="Valider", command=self.valider_callback)
+        bouton_valider = ttk.Button(self.frame_center, text="Valider", command=None)
         bouton_valider.pack(pady=10)
 
 
@@ -76,41 +76,14 @@ class MyWindow_connection:
             widget.config(fg='black')  # Changer la couleur du texte en noir
 
     # Méthode pour restaurer le texte initial si le champ de saisie est vide
-    # def restore_default_text(self, event):
-    #     widget = event.widget
-    #     initial_text = "Nom d'utilisateur" if widget == self.entry_nom_utilisateur else "Mot de passe"
+    def restore_default_text(self, event):
+        widget = event.widget
+        initial_text = "Nom d'utilisateur" if widget == self.entry_nom_utilisateur else "Mot de passe"
 
-    #     if not widget.get():
-    #         widget.insert(0, initial_text)
-    #         widget.config(fg='grey')  # Changer la couleur du texte en gris clair
+        if not widget.get():
+            widget.insert(0, initial_text)
+            widget.config(fg='grey')  # Changer la couleur du texte en gris clair
 
-    #     def rechercher_callback(self):
-    #         site_recherche = self.entry_site.get()
-    #     data = self.user_data_manager.get_user_data_by_site(site_recherche)
-
-    #     if data:
-    #         messagebox.showinfo("Résultat de la Recherche", f"Informations trouvées pour le site '{site_recherche}':\n"
-    #                                                          f"Identifiant: {data.get('identifiant', 'N/A')}\n"
-    #                                                          f"Email: {data.get('email', 'N/A')}\n"
-    #                                                          f"Mot de passe: {data.get('mot_de_passe', 'N/A')}")
-    #     else:
-    #         messagebox.showinfo("Résultat de la Recherche", f"Aucune information trouvée pour le site '{site_recherche}'.")
-
-    
-    # Méthode pour supprimer les données liées au site
-    def supprimer_callback(self):
-        site_supprimer = self.entry_site.get()
-
-        if site_supprimer:
-            # Appeler la nouvelle méthode pour supprimer les données liées au site
-            success = self.user_data_manager.delete_user_data_by_site(site_supprimer)
-
-            if success:
-                messagebox.showinfo("Suppression réussie", f"Données pour le site '{site_supprimer}' supprimées avec succès.")
-            else:
-                messagebox.showinfo("Aucune correspondance", f"Aucune information trouvée pour le site '{site_supprimer}'.")
-        else:
-            messagebox.showinfo("Entrée vide", "Veuillez entrer un nom de site pour supprimer les données correspondantes.")
 
 def valider_callback(self):
         # Récupérer les valeurs des champs de saisie
--- Branch_page/Admin_création_Données/Gestion_data_user.py
+++ Branch_page/Admin_création_Données/Gestion_data_user.py
@@ -4,12 +4,22 @@ import os
 class GestionData:
 
     def save_data(self, data):
+        # Chemin du fichier JSON
+        file_path = 'C:/Data/MainData.json'
+
         # Charger les données existantes (si le fichier existe)
-        if os.path.exists(Data.json):
-            with open(, 'r') as file:
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as file:
                 existing_data = json.load(file)
         else:
             existing_data = {}
+
+        # Mettre à jour les données existantes avec les nouvelles données
+        existing_data.update(data)
+
+        # Écrire les données mises à jour dans le fichier JSON
+        with open(file_path, 'w') as file:
+            json.dump(existing_data, file)
 
         # Utiliser le site comme clé principale
         nom_oeuvre = data.get('entry_nom_oeuvre')
@@ -29,43 +39,3 @@ class GestionData:
         # Écrire les données dans le fichier
         with open(self.user_data_filename, 'w') as file:
            json.dump(existing_data, file, indent=4)
-
-    def load_user_data(self):
-        # Charger les données existantes (si le fichier existe)
-        if os.path.exists(self.user_data_filename):
-            with open(self.user_data_filename, 'r') as file:
-                user_data = json.load(file)
-
-            # Triez les données par le site
-            sorted_data = sorted(user_data.items(), key=lambda x: x[0])
-
-            return [data[1] for data in sorted_data]
-        else:
-            return []
-        
-    def get_user_data_by_site(self, site):
-        if os.path.exists(self.user_data_filename):
-            with open(self.user_data_filename, 'r') as file:
-                user_data = json.load(file)
-            return user_data.get(site, {})
-        else:
-            return {}
-        
-    def delete_user_data_by_site(self, site):
-        try:
-            # Charger les données existantes
-            user_data = self.load_user_data()
-
-            # Vérifier si le site existe dans les données
-            if site in user_data:
-                # Supprimer les données liées au site
-                del user_data[site]
-
-                # Enregistrer les données mises à jour
-                self.save_user_data(user_data)
-                return True
-            else:
-                # Le site n'existe pas dans les données
-                return False
-        except Exception as e:
-            return False
