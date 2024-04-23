9a8e88991d3226200eb97e6ed53bdbb4e4dc23f4
HugoSchuli <hugoschuliar@gmail.com>
2024-04-23 09:14:33 +0200

    ajout livre
    fonctionne pas

---
+++ nouveau_livre.php
@@ -0,0 +1,23 @@
+<html>
+       <head>
+
+                <title>method post</title>
+
+        </head>
+
+
+        <body>
+
+                <?php
+
+                $titre=$_POST['Titre']
+
+                $auteur=$_POST['auteur']
+
+                $lien=$_POST['url']
+
+                $description=$_POST['Description']
+                ?>
+        </body>
+</html>
+
