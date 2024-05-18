234dd69ceb1d6929be6d45e109cdf646d904354a
Thomas.pl <145576069+Waser92@users.noreply.github.com>
2024-05-18 16:52:29 +0200

    Update

--- Source/page_livres.js
+++
@@ -1,9 +0,0 @@
-function envoyerMail() {
-    var email = document.getElementById("email").value;
-    var subject = "Sujet de l'e-mail";
-    var body = "Contenu de l'e-mail";
-
-    var mailto_link = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
-    win = window.open(mailto_link, 'emailWindow');
-    if (win && win.open && !win.closed) win.close();
-}
